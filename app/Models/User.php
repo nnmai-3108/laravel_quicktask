0cdf4db77963bfed20e3c06336c498b771e8c049
nnmai-3108 <harumiaki3108@gmail.com>
2022-08-16 22:45:27 +0700

    chapter4

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -47,5 +48,17 @@ class User extends Authenticatable
     public function tasks()
     {
         return $this->hasMany(Task::class);
+    }
+
+    // tạo accessors
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+    // tạo Mutators
+    public function setUserNameAttribute($username)
+    {
+        $this->attributes['username'] = Str::slug($username);
     }
 }
