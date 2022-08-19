449ef2fb554ee8d14404ddc4f59a9128160b2743
nnmai-3108 <harumiaki3108@gmail.com>
2022-08-19 15:05:28 +0700

    chapter8

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CheckAdmin
 {
@@ -16,8 +17,7 @@ class CheckAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        $user = $request->user();
-        if ($user->is_admin) {
+        if (Auth::user() && Auth::user()->is_admin) {
             return $next($request);
         }
 
